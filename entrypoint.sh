8a249d284fb7838a07f6d336061ab41c41f9b424
RakeshKhadav <rakeshkhadav47@gmail.com>
2026-06-05 22:33:26 +0530

    fix: update entrypoint script to use temporary log file for Prisma migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 echo "--- RUNNING PRISMA MIGRATIONS ---"
-if ! prisma migrate deploy > prisma_migrate.log 2>&1; then
+if ! prisma migrate deploy > /tmp/prisma_migrate.log 2>&1; then
   echo "Prisma migrations failed with the following error:" >&2
-  cat prisma_migrate.log >&2
+  cat /tmp/prisma_migrate.log >&2
   exit 1
 fi
 echo "Prisma migrations ran successfully:"
-cat prisma_migrate.log
+cat /tmp/prisma_migrate.log
 
 echo "--- STARTING NEXT.JS SERVER ---"
 exec node server.js
